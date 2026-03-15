84f1c53231c790a4d55f6e398e58813c7486f433
LeHoangMy <lhmy11297@gmail.com>
2026-03-15 22:29:24 +0700

    Project 05: Glamira web crawling pipeline

--- Le_Hoang_My_LV1_project_05/draft.py
+++
@@ -1,9 +0,0 @@
-from IP2Location import IP2Location
-
-reader = IP2Location("data/IP-COUNTRY-REGION-CITY.BIN")
-rec = reader.get_all("37.170.17.183")
-
-for attr in dir(rec):
-    if not attr.startswith("_"):
-        val = getattr(rec, attr)
-        print(f"{attr}: {val}")
